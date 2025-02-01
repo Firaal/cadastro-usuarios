87b86eef73fac9bd1bc4f40f218020722ed2bd5e
Anderson Bernardo <anderson.bernardo508@gmail.com>
2025-02-01 02:42:06 -0300

    Adicionado arquivo para a conexão com o banco de dados

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,47 +1,39 @@
 import "./style.css";
 import Trash from "../../assets/trash.svg";
+import api from "../../services/api";
 
 function Home() {
-    const users = [
-        {
-            id: 1,
-            name: "Paulo",
-            age: 20,
-            email: "rod@email.com",
-        },
-        {
-            id: 2,
-            name: "Matheus",
-            age: 22,
-            email: "matheus22@email.com",
-        },
-    ];
+    let videos = [];
+
+    async function getVideos() {
+        videos = await api.get("/videos");
+    }
 
     return (
         <div className="container">
             <form action="" className="container__form">
-                <h1 className="form__title">Cadastro de usuários</h1>
+                <h1 className="form__title">Cadastro de videos</h1>
 
-                <input type="text" name="nome" className="form__input" placeholder="Nome" />
-                <input type="number" name="idade" className="form__input" placeholder="Idade" />
-                <input type="email" name="email" className="form__input" placeholder="Email" />
+                <input type="text" name="title" className="form__input" placeholder="Titulo" />
+                <input type="text" name="description" className="form__input" placeholder="Descrição" />
+                <input type="number" name="duration" className="form__input" placeholder="Duração" />
 
                 <button type="button" className="form__button">
                     Cadastrar
                 </button>
             </form>
 
-            {users.map((user) => (
-                <div key={user.id} className="container__card">
+            {videos.map((video) => (
+                <div className="container__card">
                     <div>
                         <p>
-                            Nome: <span>{user.name}</span>
+                            Titulo: <span>{video.title}</span>
                         </p>
                         <p>
-                            Idade: <span>{user.age}</span>
+                            Descrição: <span>{video.description}</span>
                         </p>
                         <p>
-                            Email: <span>{user.email}</span>
+                            Duração: <span>{video.duration}</span>
                         </p>
                     </div>
 
