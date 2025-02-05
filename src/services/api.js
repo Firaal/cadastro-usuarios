44f55469ab8c7ecaa817125164e73b8e91ac5c6c
Anderson Bernardo <anderson.bernardo508@gmail.com>
2025-02-05 18:49:04 -0300

    Alterando url de requisição

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3333",
+    baseURL: "https://node-do-zero-6x05.onrender.com",
     headers: {
         "Content-Type": "application/json",
     },
